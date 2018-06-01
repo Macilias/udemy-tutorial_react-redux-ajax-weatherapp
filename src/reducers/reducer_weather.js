981ba213211bdfc5fa7b1287d40c2cfe8e10c64b
Maciek Niemczyk <Maciek.Niemczyk@vitraum.de>
2018-06-01 17:24:22 +0200

    added charts which shows temp, pressure and humidity

--- src/reducers/reducer_weather.js
+++ src/reducers/reducer_weather.js
@@ -1,3 +1,14 @@
-export default function (state = null, action) {
+import { FETCH_WEATHER } from "../actions";
+
+export default function (state = [], action) {
+	switch (action.type) {
+		case FETCH_WEATHER:
+			// super important, concat returns a new copy with the element added!
+			// this way we do not manipulate the state and are conform with react & redux
+			// return state.concat([ action.payload.data ]);
+			// alternatively (ES6 Syntax) creates also a new one and adds elements to it
+			// the only difference is, this one inserts state at the front of the array.
+			return [ action.payload.data, ...state ]
+	}
 	return state;
 }
